f5673d6aeaccc925a97875c589ab00cb294b0a2f
himawari <1156544355@qq.com>
2022-10-18 23:56:31 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+  <div id="bind-attribute">
+    <span v-bind:title="message">
+      鼠标悬停几秒钟查看此处动态绑定的提示信息！
+    </span>
+    <div id="event-handling">
+      <p>{{ message }}</p>
+      <button v-on:click="reverseMessage">反转 Message</button>
+    </div>
+    <div id="two-way-binding">
+      <p>{{ message2 }}</p>
+      <input v-model="message2" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+      message: "You loaded this page on " + new Date().toLocaleString(),
+      message2: "hello world"
+    };
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+};
+</script>
