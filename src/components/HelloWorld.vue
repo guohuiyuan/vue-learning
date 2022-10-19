c722b06c32a5a6a8366d30139f5002da446d5466
himawari <1156544355@qq.com>
2022-10-19 08:42:38 +0800

    🎨 添加例子

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,35 @@
     </div>
     <div id="two-way-binding">
       <p>{{ message2 }}</p>
+      <p>message2</p>
       <input v-model="message2" />
+    </div>
+    <div id="conditional-rendering">
+      <span v-if="!!message3">现在你看到我了</span>
+      <p>message3</p>
+      <input v-model="message3" />
+    </div>
+    <div id="list-rendering">
+      <ol>
+        <li v-for="todo in todos" :key="todo">
+          {{ todo.text }}
+        </li>
+      </ol>
+    </div>
+    <div id="todo-list-app">
+      <ol>
+        <!--
+      现在我们为每个 todo-item 提供 todo 对象
+      todo 对象是变量，即其内容可以是动态的。
+      我们也需要为每个组件提供一个“key”，稍后再
+      作详细解释。
+    -->
+        <todo-item
+          v-for="item in groceryList"
+          v-bind:todo="item"
+          v-bind:key="item.id"
+        ></todo-item>
+      </ol>
     </div>
   </div>
 </template>
@@ -29,7 +57,18 @@ export default {
     return {
       count: 0,
       message: "You loaded this page on " + new Date().toLocaleString(),
-      message2: "hello world"
+      message2: "hello world",
+      message3: "",
+      todos: [
+        { text: "Learn JavaScript" },
+        { text: "Learn Vue" },
+        { text: "Build something awesome" },
+      ],
+       groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ],
     };
   },
   methods: {
---
+++ src/components/Item.vue
@@ -0,0 +1,17 @@
+<template>
+    <li>{{ todo.text }}</li>
+</template>
+
+<script>
+export default {
+  name: "Item",
+  props: {
+    todo: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
